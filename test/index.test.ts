eb877c8d9367b179404e6c21ff4d5310648118ed
Garrett Stevens <stevens.garrett.j@gmail.com>
2021-12-15 15:16:01 -0700

    Add a couple more tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,64 +1,74 @@
 import { parseBreakend, Breakend } from '../src'
 
-test('can parse breakends', () => {
-  // Breakends from https://samtools.github.io/hts-specs/VCFv4.3.pdf
-  const breakendsAndParsed = [
-    [
-      'G]17:198982]',
-      {
-        MatePosition: '17:198982',
-        Join: 'right',
-        Replacement: 'G',
-        MateDirection: 'left',
-      },
-    ],
-    [
-      ']13:123456]T',
-      {
-        MatePosition: '13:123456',
-        Join: 'left',
-        Replacement: 'T',
-        MateDirection: 'left',
-      },
-    ],
-    [
-      'C[2:321682[',
-      {
-        MatePosition: '2:321682',
-        Join: 'right',
-        Replacement: 'C',
-        MateDirection: 'right',
-      },
-    ],
-    [
-      '[17:198983[A',
-      {
-        MatePosition: '17:198983',
-        Join: 'left',
-        Replacement: 'A',
-        MateDirection: 'right',
-      },
-    ],
-    [
-      'A]2:321681]',
-      {
-        MatePosition: '2:321681',
-        Join: 'right',
-        Replacement: 'A',
-        MateDirection: 'left',
-      },
-    ],
-    [
-      '[13:123457[C',
-      {
-        MatePosition: '13:123457',
-        Join: 'left',
-        Replacement: 'C',
-        MateDirection: 'right',
-      },
-    ],
-  ] as [string, Breakend][]
-  breakendsAndParsed.forEach(([breakend, parsedBreakend]) => {
-    expect(parseBreakend(breakend)).toEqual(parsedBreakend)
+describe('testBreakend', () => {
+  it('can parse breakends', () => {
+    // Breakends from https://samtools.github.io/hts-specs/VCFv4.3.pdf
+    const breakendsAndParsed = [
+      [
+        'G]17:198982]',
+        {
+          MatePosition: '17:198982',
+          Join: 'right',
+          Replacement: 'G',
+          MateDirection: 'left',
+        },
+      ],
+      [
+        ']13:123456]T',
+        {
+          MatePosition: '13:123456',
+          Join: 'left',
+          Replacement: 'T',
+          MateDirection: 'left',
+        },
+      ],
+      [
+        'C[2:321682[',
+        {
+          MatePosition: '2:321682',
+          Join: 'right',
+          Replacement: 'C',
+          MateDirection: 'right',
+        },
+      ],
+      [
+        '[17:198983[A',
+        {
+          MatePosition: '17:198983',
+          Join: 'left',
+          Replacement: 'A',
+          MateDirection: 'right',
+        },
+      ],
+      [
+        'A]2:321681]',
+        {
+          MatePosition: '2:321681',
+          Join: 'right',
+          Replacement: 'A',
+          MateDirection: 'left',
+        },
+      ],
+      [
+        '[13:123457[C',
+        {
+          MatePosition: '13:123457',
+          Join: 'left',
+          Replacement: 'C',
+          MateDirection: 'right',
+        },
+      ],
+    ] as [string, Breakend][]
+    breakendsAndParsed.forEach(([breakend, parsedBreakend]) => {
+      expect(parseBreakend(breakend)).toEqual(parsedBreakend)
+    })
+  })
+
+  it('throws on invalid breakend', () => {
+    expect(() => parseBreakend('[13:123457[')).toThrow(/Invalid breakend/)
+  })
+
+  it('returns "undefined" for non-breakend', () => {
+    expect(parseBreakend('A')).toBeUndefined()
   })
 })
