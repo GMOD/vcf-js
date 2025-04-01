e5a9ef864f1b59f3b0f60ca0e23121aff58005b2
Colin <colin.diesh@gmail.com>
2025-03-31 21:43:07 -0400

    Misc

--- src/parseGenotypesOnly.ts
+++ src/parseGenotypesOnly.ts
@@ -6,23 +6,24 @@ export function parseGenotypesOnly(
   const rest = prerest.split('\t')
   const genotypes = {} as Record<string, string>
   let i = 0
-  const formatSplit = format.split(':')
-  if (formatSplit.length === 1) {
-    for (const sample of samples) {
-      genotypes[sample] = rest[i++]!
-    }
-  } else {
-    const gtIndex = formatSplit.indexOf('GT')
-    if (gtIndex === 0) {
+  if (format.includes('GT')) {
+    const formatSplit = format.split(':')
+    if (formatSplit.length === 1) {
       for (const sample of samples) {
-        const val = rest[i++]!
-        const idx = val.indexOf(':')
-        genotypes[sample] = idx !== -1 ? val.slice(0, idx) : val
+        genotypes[sample] = rest[i++]!
       }
     } else {
-      for (const sample of samples) {
-        const val = rest[i++]!.split(':')
-        genotypes[sample] = val[gtIndex]!
+      const gtIndex = formatSplit.indexOf('GT')
+      if (gtIndex === 0) {
+        for (const sample of samples) {
+          const idx = rest[i++]!.indexOf(':')
+          genotypes[sample] = idx !== -1 ? rest[i++]!.slice(0, idx) : rest[i++]!
+        }
+      } else {
+        for (const sample of samples) {
+          const val = rest[i++]!.split(':')
+          genotypes[sample] = val[gtIndex]!
+        }
       }
     }
   }
