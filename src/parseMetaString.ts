927d627754bb47fb5114a39c263871745914318b
Colin <colin.diesh@gmail.com>
2025-11-19 18:11:07 -0500

    Small opts

--- src/parseMetaString.ts
+++ src/parseMetaString.ts
@@ -8,7 +8,7 @@
 // it undid the regex into serial logic and the result was this function
 function customSplit(str: string) {
   const result = []
-  let current = ''
+  const chars = []
   let inQuotes = false
   let inBrackets = false
   const strLen = str.length
@@ -17,23 +17,23 @@ function customSplit(str: string) {
     const char = str[i]!
     if (char === '"') {
       inQuotes = !inQuotes
-      current += char
+      chars.push(char)
     } else if (char === '[') {
       inBrackets = true
-      current += char
+      chars.push(char)
     } else if (char === ']') {
       inBrackets = false
-      current += char
+      chars.push(char)
     } else if (char === ',' && !inQuotes && !inBrackets) {
-      result.push(current.trim())
-      current = ''
+      result.push(chars.join('').trim())
+      chars.length = 0
     } else {
-      current += char
+      chars.push(char)
     }
   }
 
-  if (current) {
-    result.push(current.trim())
+  if (chars.length > 0) {
+    result.push(chars.join('').trim())
   }
 
   return result
@@ -45,7 +45,7 @@ function splitFirst(str: string, split: string) {
 }
 
 export function parseMetaString(metaString: string) {
-  const inside = metaString.replace(/^<|>$/g, '')
+  const inside = metaString.slice(1, -1)
   const parts = customSplit(inside)
   const entries: [string, any][] = []
   for (let i = 0; i < parts.length; i++) {
@@ -53,15 +53,14 @@ export function parseMetaString(metaString: string) {
     const [key, val] = splitFirst(f, '=')
     if (val && val.startsWith('[') && val.endsWith(']')) {
       const items = val.slice(1, -1).split(',')
-      const trimmed: string[] = []
       for (let j = 0; j < items.length; j++) {
-        trimmed.push(items[j]!.trim())
+        items[j] = items[j]!.trim()
       }
-      entries.push([key, trimmed])
+      entries.push([key, items])
     } else if (val && val.startsWith('"') && val.endsWith('"')) {
       entries.push([key, val.slice(1, -1)])
     } else {
-      entries.push([key, val?.replaceAll(/^"|"$/g, '')])
+      entries.push([key, val])
     }
   }
   return Object.fromEntries(entries)
