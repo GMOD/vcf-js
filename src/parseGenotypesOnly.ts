fb2ee356fb9729f44cf26c686349231c4f9634e1
Colin <colin.diesh@gmail.com>
2025-11-20 20:31:32 -0500

    Bench

--- src/parseGenotypesOnly.ts
+++ src/parseGenotypesOnly.ts
@@ -9,19 +9,25 @@ export function parseGenotypesOnly(
     return genotypes
   }
 
-  const rest = prerest.split('\t')
   const samplesLen = samples.length
-  let i = 0
+  const prerestLen = prerest.length
+  let pos = 0
 
   if (format.length === 2) {
     for (let idx = 0; idx < samplesLen; idx++) {
-      genotypes[samples[idx]!] = rest[i++]!
+      const start = pos
+      while (pos < prerestLen && prerest[pos] !== '\t') pos++
+      genotypes[samples[idx]!] = prerest.slice(start, pos)
+      pos++
     }
   } else if (gtIdx === 0) {
     for (let idx = 0; idx < samplesLen; idx++) {
-      const val = rest[i++]!
-      const colonIdx = val.indexOf(':')
-      genotypes[samples[idx]!] = colonIdx !== -1 ? val.slice(0, colonIdx) : val
+      const start = pos
+      while (pos < prerestLen && prerest[pos] !== ':' && prerest[pos] !== '\t')
+        pos++
+      genotypes[samples[idx]!] = prerest.slice(start, pos)
+      while (pos < prerestLen && prerest[pos] !== '\t') pos++
+      pos++
     }
   } else {
     let colonCount = 0
@@ -31,20 +37,24 @@ export function parseGenotypesOnly(
       }
     }
     for (let idx = 0; idx < samplesLen; idx++) {
-      const val = rest[i++]!
+      const sampleStart = pos
+      let tabIdx = pos
+      while (tabIdx < prerestLen && prerest[tabIdx] !== '\t') tabIdx++
+      const val = prerest.slice(sampleStart, tabIdx)
       const valLen = val.length
       let colons = 0
-      let start = 0
+      let fieldStart = 0
       for (let j = 0; j <= valLen; j++) {
         if (j === valLen || val[j] === ':') {
           if (colons === colonCount) {
-            genotypes[samples[idx]!] = val.slice(start, j)
+            genotypes[samples[idx]!] = val.slice(fieldStart, j)
             break
           }
           colons++
-          start = j + 1
+          fieldStart = j + 1
         }
       }
+      pos = tabIdx + 1
     }
   }
 
