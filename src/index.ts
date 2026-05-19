38634b2a68cb128e9b92beb6fbd394e080adcb7f
Colin <colin.diesh@gmail.com>
2026-05-18 23:35:14 -0400

    fix lint order

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Variant } from './Variant.ts'
-export type { SampleValue, SampleData, Samples } from './Variant.ts'
+export type { SampleData, SampleValue, Samples } from './Variant.ts'
 export type { GenotypeCallback } from './processGenotypes.ts'
 export type { InfoValue, MetaField, MetaMap } from './parseInfo.ts'
 
