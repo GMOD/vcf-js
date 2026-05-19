a58b19565ce927a200eba3cdffd542a5394cb459
Colin <colin.diesh@gmail.com>
2026-05-18 23:34:48 -0400

    feat: export Samples, InfoValue, MetaField, MetaMap types
    Lets downstream consumers' inferred toJSON / getMetadata return types stay
    nameable (avoids TS2883 'cannot be named without a reference' errors).

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { Variant } from './Variant.ts'
+export type { SampleValue, SampleData, Samples } from './Variant.ts'
 export type { GenotypeCallback } from './processGenotypes.ts'
+export type { InfoValue, MetaField, MetaMap } from './parseInfo.ts'
 
 export { default } from './parse.ts'
 export * from './parseBreakend.ts'
